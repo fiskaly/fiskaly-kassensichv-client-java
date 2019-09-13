3f10d1d4375a1d34f35bdb6ab1f41fd72b8ef3de
prempador <muellner.benjamin@gmail.com>
2019-09-13 15:57:04 +0200

    Library Files for sma-general are now loaded into the jar correctly. Added GeneralSMATest as the first Integration-Test and automated the process with TestRunner.sh shell script.

---
+++ sma-general/src/test/java/GeneralSMATest.java
@@ -0,0 +1,32 @@
+import com.fiskaly.kassensichv.sma.GeneralSMA;
+
+import java.io.IOException;
+
+
+// javac -cp "sma-general-0.0.1-alpha.jar:sma-common-0.0.1-alpha.jar" GeneralSMATest.java
+
+public class GeneralSMATest {
+
+    public static GeneralSMA generalSMA = null;
+
+    public static void main(String[] args) {
+
+        try{
+            generalSMA = new GeneralSMA();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        invokeResponseTest();
+    }
+
+    private static void invokeResponseTest() {
+
+        String invokeResponse = generalSMA.invoke("{\"jsonrpc\":\"2.0\",\"method\":\"version\"}");
+
+        if(invokeResponse != null && !invokeResponse.equals("")){
+            System.out.println(invokeResponse);
+        }
+    }
+
+}
