0c367c39228c47235367674ab3434bf51103db27
Benjamin Auinger <benjamin.au.99@gmx.at>
2019-09-23 13:59:29 +0200

    shorten test

--- client/src/test/java/GeneralSMATest.java
+++ client/src/test/java/GeneralSMATest.java
@@ -1,30 +1,14 @@
 import com.fiskaly.kassensichv.sma.GeneralSMA;
 
-import java.io.IOException;
-
 public class GeneralSMATest {
+    public static void main(String[] args) throws Exception {
+        GeneralSMA sma = new GeneralSMA();
+        String response = sma.invoke("{\"jsonrpc\":\"2.0\",\"method\":\"version\"}");
 
-    public static GeneralSMA generalSMA = null;
-
-    public static void main(String[] args) throws Exception{
-
-        System.out.println("GeneralSMATest");
-
-        generalSMA = new GeneralSMA();
-
-        invokeResponseTest();
-    }
-
-    private static void invokeResponseTest() {
-
-        String invokeResponse = generalSMA.invoke("{\"jsonrpc\":\"2.0\",\"method\":\"version\"}");
-
-        if(invokeResponse == null || invokeResponse.equals("")){
-            System.err.println("Error in Test \"invokeResponseTest\": \nGot Null or Empty return value.");
+        if (response == null || response.isEmpty()) {
             System.exit(1);
-        } else {
-            System.out.println("Test \"invokeResponseTest\" completed successfully.");
         }
-    }
 
+        System.exit(0);
+    }
 }
