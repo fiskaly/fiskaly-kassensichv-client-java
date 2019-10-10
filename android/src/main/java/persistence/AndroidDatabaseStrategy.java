63f2ed2949a2c5c0b3c0c33f8a6b8a8142920969
Benjamin Auinger <benjamin.au.99@gmx.at>
2019-10-10 16:23:54 +0200

    Add not implement exceptions to Android

--- android/src/main/java/persistence/AndroidDatabaseStrategy.java
+++ android/src/main/java/persistence/AndroidDatabaseStrategy.java
@@ -1,6 +1,20 @@
 package persistence;
 
-public class AndroidDatabaseStrategy {
-    public AndroidDatabaseStrategy() {
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.IOException;
+import java.util.List;
+
+public class AndroidDatabaseStrategy implements PersistenceStrategy{
+    public AndroidDatabaseStrategy() {}
+
+    @Override
+    public void persistRequest(Request request) throws IOException {
+       throw new NotImplementedException();
+    }
+
+    @Override
+    public List<Request> loadRequests() throws IOException {
+        throw new NotImplementedException();
     }
 }
